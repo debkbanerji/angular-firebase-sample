64c5cbab9c37d2456e419619ed04d4f4fbd9e2fd
deb <debkbanerji@gmail.com>
2017-06-06 20:57:52 -0400

    Sending Friend Requests
    Implemented sending friend requests

--- src/app/add-friends/add-friends.component.ts
+++ src/app/add-friends/add-friends.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
 import {AuthService} from '../providers/auth.service';
 import {NgForm} from '@angular/forms';
 import {Subscription} from 'rxjs/Subscription';
@@ -14,6 +14,10 @@ export class AddFriendsComponent implements OnInit, OnDestroy {
     private searchResults: FirebaseListObservable<any[]>;
     private searchSubscription: Subscription;
     private userUID: string;
+    private friendObject: FirebaseObjectObservable<any>;
+    private checkFriendSubscription: Subscription;
+    private friendProfileObject: FirebaseObjectObservable<any>;
+    private friendProfileSubscription: Subscription;
 
     constructor(public authService: AuthService, private db: AngularFireDatabase) {
     }
@@ -55,13 +59,60 @@ export class AddFriendsComponent implements OnInit, OnDestroy {
         if (this.userUID === friendUID) {
             this.searchText = 'You can\'t send a friend request to yourself';
         } else {
-            // TODO: Send Friend Request
+            this.friendObject = this.db.object('friend-lists/' + this.userUID + '/' + friendUID);
+            this.checkFriendSubscription = this.friendObject.subscribe((data) => {
+                if (data.$value !== null) {
+                    this.searchText = 'Friend request already sent';
+                    console.log(data);
+                } else {
+                    // TODO: Send Friend Request
+                    this.searchText = 'Friend request sent';
+                    const chatKey = this.makeChat(friendUID, this.userUID);
+
+                    this.checkFriendSubscription.unsubscribe();
+                    this.friendProfileObject = this.db.object('user-profiles/' + friendUID);
+                    this.friendProfileSubscription = this.friendProfileObject.subscribe((friendData) => {
+                        friendData['chat-key'] = chatKey;
+                        let friendRequestObject;
+                        this.friendObject.set(friendData);
+                        friendData['accepted'] = false;
+                        friendRequestObject = this.db.object('friend-requests/' + this.userUID + '/' + friendUID);
+                        friendRequestObject.set(friendData);
+
+                        this.friendProfileSubscription.unsubscribe();
+                        this.checkFriendSubscription.unsubscribe();
+                    });
+                }
+            });
         }
+    }
+
+
+    private makeChat(uid1, uid2) {
+        if (uid1 > uid2) {
+            let temp;
+            temp = uid1;
+            uid1 = uid2;
+            uid2 = temp;
+        }
+        let chatKey;
+        chatKey = this.db.list('/chats').push({
+            uid1: uid1,
+            uid2: uid2
+        }).key;
+        // console.log(chatKey);
+        return chatKey;
     }
 
     ngOnDestroy(): void {
         if (this.searchSubscription) {
             this.searchSubscription.unsubscribe();
+        }
+        if (this.checkFriendSubscription) {
+            this.checkFriendSubscription.unsubscribe();
+        }
+        if (this.friendProfileSubscription) {
+            this.friendProfileSubscription.unsubscribe();
         }
     }
 }
